55e4f7a37b277f7bb2401b5d7bfead7280180694
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-15 10:18:44 +0100

    Add project files.

---
+++ moodleOppgave10/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace moodleOppgave10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Terje er kul");
+            }
+        }
+    }
+}
